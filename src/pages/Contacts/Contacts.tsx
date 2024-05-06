18ac3b7f57b42386d9646434f404f92328ee6213
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-05-06 20:58:22 +0300

    contacts page

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+// import React from 'react'
+import { Box} from '@mui/material';
+import { rows, columns } from './data'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <Box sx={{ height: 600, width: '100%', mx: "auto" }}>
+      <DataGrid 
+      slots={{
+        toolbar: GridToolbar,
+      }}
+      rows={rows} columns={columns} />
+    </Box>
   )
 }
--- src/pages/Teams/Teams.tsx
+++ src/pages/Teams/Teams.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+// import React from 'react'
+import { rows } from './data'
+import { Box, Typography, useTheme } from '@mui/material';
+import { AdminPanelSettingsOutlined, LockOpenOutlined, SecurityOutlined } from '@mui/icons-material';
+
+
+
+
 
 export default function Teams() {
+  const theme = useTheme()
+
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", align: "center", headerAlign: "center" },
+    { field: "name", headerName: "Name", align: "center", headerAlign: "center", flex: 1 },
+    { field: "email", headerName: "Email", align: "center", headerAlign: "center", flex: 1 },
+    { field: "age", headerName: "Age", align: "center", headerAlign: "center" },
+    { field: "phone", headerName: "Phone", align: "center", headerAlign: "center", flex: 1 },
+    {
+      field: "access", headerName: "acess", align: "center", headerAlign: "center", flex: 1
+      ///////////////// I dont get these 2 lines
+      , renderCell: ({ row: { access } }) => {
+        return (
+          <Box sx={{
+            p: "5px",
+            mx: "auto",
+            // height: "100%",
+            borderRadius: "3px",
+            width: "99px",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            backgroundColor: access === "Admin"
+              ? theme.palette.primary.dark
+              : access === "Manager"
+                ? theme.palette.secondary.dark
+                : "#3da58a",
+
+          }}>
+            {access === "Admin" && (<AdminPanelSettingsOutlined fontSize='small' sx={{ color: '#fff'}}/>
+            )}
+            {access === "Manager" && (<SecurityOutlined fontSize='small' sx={{ color: '#fff'}} />
+            )}
+            {access === "User" && (<LockOpenOutlined fontSize='small' sx={{ color: '#fff'}} />
+            )}
+            <Typography sx={{ fontSize: "14px", color: '#fff'}}>{access}</Typography>
+          </Box>
+        )
+      }
+    },
+  ];
+  []
   return (
-    <div>Teams</div>
+
+    <Box sx={{ height: 600, width: '100%', mx: "auto" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </Box>
+
   )
 }
