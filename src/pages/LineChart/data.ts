a301a60bbe6a1ace086207bf0bf37e89e1d28ddf
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-05-09 05:48:54 +0300

    pie chart and line chart pages

---
+++ src/pages/LineChart/data.ts
@@ -0,0 +1,272 @@
+export const data = [
+  {
+    id: "japan",
+    color: "hsl(291, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 275,
+      },
+      {
+        x: "helicopter",
+        y: 155,
+      },
+      {
+        x: "boat",
+        y: 116,
+      },
+      {
+        x: "train",
+        y: 42,
+      },
+      {
+        x: "subway",
+        y: 200,
+      },
+      {
+        x: "bus",
+        y: 213,
+      },
+      {
+        x: "car",
+        y: 51,
+      },
+      {
+        x: "moto",
+        y: 157,
+      },
+      {
+        x: "bicycle",
+        y: 279,
+      },
+      {
+        x: "horse",
+        y: 141,
+      },
+      {
+        x: "skateboard",
+        y: 1,
+      },
+      {
+        x: "others",
+        y: 235,
+      },
+    ],
+  },
+  {
+    id: "france",
+    color: "hsl(3, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 267,
+      },
+      {
+        x: "helicopter",
+        y: 159,
+      },
+      {
+        x: "boat",
+        y: 20,
+      },
+      {
+        x: "train",
+        y: 287,
+      },
+      {
+        x: "subway",
+        y: 23,
+      },
+      {
+        x: "bus",
+        y: 179,
+      },
+      {
+        x: "car",
+        y: 186,
+      },
+      {
+        x: "moto",
+        y: 38,
+      },
+      {
+        x: "bicycle",
+        y: 129,
+      },
+      {
+        x: "horse",
+        y: 152,
+      },
+      {
+        x: "skateboard",
+        y: 188,
+      },
+      {
+        x: "others",
+        y: 155,
+      },
+    ],
+  },
+  {
+    id: "us",
+    color: "hsl(54, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 125,
+      },
+      {
+        x: "helicopter",
+        y: 50,
+      },
+      {
+        x: "boat",
+        y: 94,
+      },
+      {
+        x: "train",
+        y: 39,
+      },
+      {
+        x: "subway",
+        y: 205,
+      },
+      {
+        x: "bus",
+        y: 87,
+      },
+      {
+        x: "car",
+        y: 247,
+      },
+      {
+        x: "moto",
+        y: 125,
+      },
+      {
+        x: "bicycle",
+        y: 40,
+      },
+      {
+        x: "horse",
+        y: 152,
+      },
+      {
+        x: "skateboard",
+        y: 51,
+      },
+      {
+        x: "others",
+        y: 165,
+      },
+    ],
+  },
+  {
+    id: "germany",
+    color: "hsl(25, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 266,
+      },
+      {
+        x: "helicopter",
+        y: 19,
+      },
+      {
+        x: "boat",
+        y: 288,
+      },
+      {
+        x: "train",
+        y: 153,
+      },
+      {
+        x: "subway",
+        y: 27,
+      },
+      {
+        x: "bus",
+        y: 33,
+      },
+      {
+        x: "car",
+        y: 194,
+      },
+      {
+        x: "moto",
+        y: 199,
+      },
+      {
+        x: "bicycle",
+        y: 161,
+      },
+      {
+        x: "horse",
+        y: 225,
+      },
+      {
+        x: "skateboard",
+        y: 93,
+      },
+      {
+        x: "others",
+        y: 178,
+      },
+    ],
+  },
+  {
+    id: "norway",
+    color: "hsl(193, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 137,
+      },
+      {
+        x: "helicopter",
+        y: 144,
+      },
+      {
+        x: "boat",
+        y: 218,
+      },
+      {
+        x: "train",
+        y: 32,
+      },
+      {
+        x: "subway",
+        y: 239,
+      },
+      {
+        x: "bus",
+        y: 235,
+      },
+      {
+        x: "car",
+        y: 207,
+      },
+      {
+        x: "moto",
+        y: 137,
+      },
+      {
+        x: "bicycle",
+        y: 126,
+      },
+      {
+        x: "horse",
+        y: 191,
+      },
+      {
+        x: "skateboard",
+        y: 218,
+      },
+      {
+        x: "others",
+        y: 134,
+      },
+    ],
+  },
+];
---
+++ src/pages/PieChart/data.ts
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 550,
+    color: "hsl(69, 70%, 50%)",
+  },
+  {
+    id: "scala",
+    label: "scala",
+    value: 271,
+    color: "hsl(358, 70%, 50%)",
+  },
+  {
+    id: "make",
+    label: "make",
+    value: 328,
+    color: "hsl(50, 70%, 50%)",
+  },
+  {
+    id: "css",
+    label: "css",
+    value: 50,
+    color: "hsl(230, 70%, 50%)",
+  },
+  {
+    id: "php",
+    label: "php",
+    value: 183,
+    color: "hsl(225, 70%, 50%)",
+  },
+];
